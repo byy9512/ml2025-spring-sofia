801c4eaf9b7046ca300ff7a146f07636e46745a9
Yuyang Bing <darylbing1220@gmail.com>
2025-05-01 13:24:45 -0700

    Applied function to the program

--- module4.py
+++ module4.py
@@ -5,15 +5,25 @@ n = int(input("Please enter N: "))
 # Collect inputs from the user
 numbers = []
 for i in range(n):
-    num = int(input(f"Please enter number {i+1}: "))
+    num = int(input(f"Please enter positive integer {i+1}: "))
     numbers.append(num)
 
 # Collect target
 X = int(input("Please enter X: "))
 
-# Validate if target exists and return the position
-if X in numbers:
-    print(numbers.index(X) + 1)
-else:
+# Validate if the target exists and return the position
+def get_index(numbers, X):
+    if X in numbers:
+        return numbers.index(X)
+    else:
+        return -1
+
+index = get_index(numbers, X)
+
+if index == -1:
     print(-1)
+else:
+    print(index + 1)
+
+
 
